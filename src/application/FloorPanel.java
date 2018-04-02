16a4b8ca98d1061fbe8c62f32d9a0c848b3ef6d2
CS-Labs <cseely@unm.edu>
2018-04-02 13:39:33 -0600

    Buttons numbers were backwards.

--- src/application/FloorPanel.java
+++ src/application/FloorPanel.java
@@ -18,9 +18,9 @@ class FloorPanel extends GridPane
         this.setHgap(10);
         this.setVgap(10);
         Queue<Integer> floors = new LinkedList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15));
-        for(int r : IntStream.range(0,5).toArray())
+        for(int r : Arrays.asList(4,3,2,1,0))
         {
-            for(int c: IntStream.range(0,2).toArray())
+            for(int c: Arrays.asList(1,0))
             {
                 this.add(new FloorButton(floors.poll()),c,r);
             }
