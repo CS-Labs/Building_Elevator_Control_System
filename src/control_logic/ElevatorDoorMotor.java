7740bed0096b5255befa8b7289841838eee1c2b1
KTStephano <KTStephano@users.noreply.github.com>
2018-04-10 04:53:34 -0600

    Adds initial version of DoorControl with support for simulating all 4 cabins and their outer doors at the same time

--- src/control_logic/ElevatorDoorMotor.java
+++ src/control_logic/ElevatorDoorMotor.java
@@ -1,8 +1,59 @@
 package control_logic;
 
+import named_types.DoorStatusType;
+import named_types.Speed;
+
+import java.util.concurrent.atomic.AtomicReference;
+
 public class ElevatorDoorMotor
 {
-    //TODO: Implement me
-    ElevatorDoorMotor() {}
-    void update(double deltaSeconds) {}
+    private double _metersMoved = 0.0;
+    private double _sizeMeters = 1.0; // around 36 inches
+    private double _openPercentage = 0.0; // 0.0 means totally closed
+    private volatile Speed _speedMS = new Speed(0.0);
+    private AtomicReference<DoorStatusType> _status = new AtomicReference<>(DoorStatusType.CLOSED);
+
+    public ElevatorDoorMotor() {
+
+    }
+
+    public ElevatorDoorMotor(double sizeMeters) {
+        _sizeMeters = sizeMeters;
+    }
+
+    public void open(Speed speed) {
+        _speedMS = new Speed(Math.abs(speed.get())); // Ensure it is positive
+        _status.set(DoorStatusType.OPENING);
+    }
+
+    public void close(Speed speed) {
+        _speedMS = new Speed(-Math.abs(speed.get())); // Ensure it is negative for easier math
+        _status.set(DoorStatusType.CLOSING);
+    }
+
+    public void notifyThatPositionSensorTriggered() {
+        if (_status.get() == DoorStatusType.CLOSING) _status.set(DoorStatusType.CLOSED);
+        else if (_status.get() == DoorStatusType.OPENING) _status.set(DoorStatusType.OPENED);
+    }
+
+    public DoorStatusType getStatus() {
+        return _status.get();
+    }
+
+    public double getOpenPercentage() {
+        return _openPercentage;
+    }
+
+    public void update(double deltaSeconds) {
+        if (_status.get() == DoorStatusType.OPENING) {
+            _metersMoved += _speedMS.get() * deltaSeconds;
+            _metersMoved = Math.max(0.0, Math.min(_sizeMeters, _metersMoved));
+            _openPercentage = _metersMoved / _sizeMeters;
+        }
+        else if (_status.get() == DoorStatusType.CLOSING) {
+            _metersMoved += _speedMS.get() * deltaSeconds;
+            _metersMoved = Math.max(0.0, Math.min(_sizeMeters, _metersMoved));
+            _openPercentage = _metersMoved / _sizeMeters;
+        }
+    }
 }
--- src/named_types/Speed.java
+++ src/named_types/Speed.java
@@ -2,7 +2,7 @@ package named_types;
 
 public class Speed
 {
-    double m_Speed;
+    double m_Speed = 0.0;
     public Speed(double speed) {m_Speed = speed;}
     public double get() {return m_Speed;}
 }
