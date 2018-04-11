48dbc8af8c30e94388ecfc1ee10179ca9685e798
Mina Faltas <minas.here@gmail.com>
2018-04-10 23:19:53 -0600

    Added a simple speed profile. Might stop before target floor is reach.

--- src/control_logic/MotionControl.java
+++ src/control_logic/MotionControl.java
@@ -1,7 +1,5 @@
 package control_logic;
 
-import engine.SceneManager;
-import named_types.DirectionType;
 import named_types.FloorNumber;
 import engine.LogicEntity;
 import named_types.Speed;
@@ -11,14 +9,10 @@ class MotionControl implements LogicEntity
 
   MotorSimulation motorSimulation = new MotorSimulation();
   FloorAlignment floorAlignment = new FloorAlignment();
+  private FloorNumber floorToGoTO;
   private int lastFloor = floorAlignment.alignedIndex(motorSimulation.getLocation());//should be 1 initially
-  private FloorNumber floorToGoTO = null;
-  private MotionStatusTypes motionStatus = MotionStatusTypes.STOPPED;
-  private DirectionType direction = DirectionType.NONE;
 
-
-
-  //TODO I think the position simulation is supposed to take place inside of Motion simulation but we can update that after the demo.
+  //TODO Implement me
   @Override
   public void process(double deltaSeconds)
   {
@@ -31,23 +25,17 @@ class MotionControl implements LogicEntity
 
   private void speedUpdate()
   {
-    if(floorToGoTO == null) return;
     //Mina's speed profile should go here
     if(lastFloor > floorToGoTO.get())
     {
-      direction = DirectionType.DOWN;
-      motionStatus = MotionStatusTypes.MOVING;
-      motorSimulation.setSpeed(new Speed(5));
+      motorSimulation.setSpeed(new Speed(0.5));
     }
     else if (lastFloor < floorToGoTO.get())
     {
-      direction = DirectionType.UP;
-      motionStatus = MotionStatusTypes.MOVING;
-      motorSimulation.setSpeed(new Speed(-5));
+      motorSimulation.setSpeed(new Speed(-0.5));
     }
     else
     {
-      motionStatus = MotionStatusTypes.STOPPED;
       motorSimulation.setSpeed(new Speed(0));
     }
   }
@@ -56,16 +44,6 @@ class MotionControl implements LogicEntity
   {
     //will return the last floor it has passed, if it is currently at a floor then it will return that floor
     return new FloorNumber(lastFloor);
-  }
-
-  public MotionStatusTypes getMotionStatus()
-  {
-    return motionStatus;
-  }
-
-  public DirectionType getDirection()
-  {
-    return direction;
   }
 
   // TODO: 4/9/2018
